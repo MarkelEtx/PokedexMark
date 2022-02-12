5dc430e84115d8b2b603436f2d7a5267d82555d0
MarkelEtx <markeletxebarria77@hotmail.com>
2022-02-12 10:37:15 +0100

    HomePokemon

---
+++ PokedexMark/Components/Pokemons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Pokemons extends React.Component {
+    render() {
+        return (
+            <View >
+                <Text>Pokemon eoeoeoeoeoeo</Text>
+            </View>
+        );
+    }
+}
+
+// ...
+
+export default Pokemons;
