03925e66aa12c801a7fb76c00f09f2393615864e
MarkelEtx <markeletxebarria77@hotmail.com>
2022-02-13 20:56:33 +0100

    appBasica

--- PokedexMark/App.js
+++ PokedexMark/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Pokemons from './Components/Pokemons';
+import Details from './Components/Details';
 const appNavigator = createStackNavigator(
   {
     PokedexMark: {
       screen: Pokemons,
+    },
+    Details: {
+      screen: Details,
     },
 
   },
--- PokedexMark/Components/Pokemons.js
+++ PokedexMark/Components/Pokemons.js
@@ -1,16 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    ScrollView,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    TextInput,
+} from 'react-native';
+import DetailsS from './Details';
 
-class Pokemons extends React.Component {
-    render() {
-        return (
-            <View >
-                <Text>Pokemon eoeoeoeoeoeo</Text>
+const Pokemons = props => {
+    const [pokemons, setPokemons] = useState([]);
+    const [searchfeild, setSearchfeild] = useState('');
+
+    useEffect(() => {
+        fetchPokemons();
+    }, []);
+
+    const fetchPokemons = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+            .then(response => response.json())
+            .then(pokemons => setPokemons(pokemons.results));
+    };
+    console.log(pokemons);
+    return (
+        <View>
+            <View style={styles.searchCont}>
+                <TextInput
+                    style={styles.searchfeild}
+                    placeholder="Search Pokemons"
+                    value={searchfeild}
+                    onChangeText={value => setSearchfeild(value)}
+                />
             </View>
-        );
-    }
-}
-
-// ...
+            <ScrollView>
+                <View style={styles.container}>
+                    {pokemons
+                        .filter(pokemon =>
+                            pokemon.name.toLowerCase().includes(searchfeild.toLowerCase())
+                        )
+                        .map((pokemon, index) => {
+                            return (
+                                <TouchableOpacity
+                                    activeOpacity={0.5}
+                                    key={index}
+                                    style={styles.card}
+                                    onPress={() =>
+                                        props.navigation.navigate('Details', {
+                                            pokemon: pokemon.name,
+                                        })
+                                    }>
+                                    <Image
+                                        style={{ width: 150, height: 150 }}
+                                        source={{
+                                            uri: `https://img.pokemondb.net/sprites/red-blue/normal/${pokemon.name}.png`,
+                                        }}
+                                    />
+                                    <Text>{pokemon.name}</Text>
+                                </TouchableOpacity>
+                            );
+                        })}
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
 
 export default Pokemons;
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+    card: {
+        display: 'flex',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: 'black',
+        marginHorizontal: 20,
+        marginVertical: 10,
+    },
+    searchCont: {
+        position: 'absolute',
+        marginBottom: 70,
+        left: '20%',
+        zIndex: 1,
+        marginTop: 10,
+
+    },
+
+    searchfeild: {
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#000',
+        textAlign: 'center',
+        width: 250,
+        borderRadius: 10,
+        backgroundColor: "white"
+    },
+});
